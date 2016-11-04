af00b3b30ba39e7df49bb34f580f92fcd8fa1f1d
beomhee.lee <beomhee.lee@linecorp.com>
2016-11-04 23:29:08 +0900

    update status

--- src/any.js
+++ src/any.js
@@ -8,11 +8,11 @@ var app = express();
 
 // set SSL
 var https_options = {
-    ca: fs.readFileSync(sslConfig.CA_PATH, 'utf8'),
-    key: fs.readFileSync(sslConfig.KEY_PATH, 'utf8'),
-    cert: fs.readFileSync(sslConfig.CERT_PATH, 'utf8')
+    ca : fs.readFileSync(sslConfig.CA_PATH, 'utf8'),
+    key : fs.readFileSync(sslConfig.KEY_PATH, 'utf8'),
+    cert : fs.readFileSync(sslConfig.CERT_PATH, 'utf8')
 };
 
-https.createServer(https_options, app).listen(443, function (){
-    console.log("Any Server is Running");
+https.createServer(https_options, app).listen(443, function(){
+    console.log("Any Bot Server is Running");
 });
