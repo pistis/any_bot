ad74f397b1bd96044aea2ed1f225663151a2bedf
beomhee.lee <beomhee.lee@linecorp.com>
2016-11-05 21:58:58 +0900

    make interface for webhook

--- src/any.js
+++ src/any.js
@@ -1,10 +1,32 @@
 const https = require('https');
 const express = require('express');
 const fs = require('fs');
-
+const bodyParser = require('body-parser');
 const sslConfig = require('./config/ssl');
 
 var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/hook', function(reqeust, response){
+    response.writeHead(200, {'Content-Type' : 'text/html'});
+    response.end('<h1>Any - vamalboro@gmail.com<h1>');
+});
+
+app.post('/hook', function(request, response){
+
+    var eventObj = request.body.events[0];
+    var source = eventObj.source;
+    var message = eventObj.message;
+
+    // request log
+    console.log('======================', new Date(), '======================');
+    console.log('[request]', request.body);
+    console.log('[request source] ', eventObj.source);
+    console.log('[request message]', eventObj.message);
+
+    response.sendStatus(200);
+});
 
 // set SSL
 var https_options = {
